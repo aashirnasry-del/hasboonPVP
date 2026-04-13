d434b68be1d07aa715e4b54ce2d87ea6dadf6749
aashirnasry-del <aashirnasry@gmail.com>
2026-04-13 22:25:27 +0500

    index.js

--- index.js
+++ index.js
@@ -14,54 +14,45 @@ let bot
 let restarting = false
 
 function startBot() {
+  
   try {
     bot = mineflayer.createBot({
       host: config.server.ip,
-      port: config.server.port,
-      username: config.bot.username,
-      version: config.server.version
-    })
-
-    bot.loadPlugin(pathfinder)
-
-    bot.once('spawn', () => {
-      console.log("Bot spawned")
-
-      const mcData = minecraftData(bot.version)
+@@ -30,12 +31,14 @@
       const moves = new Movements(bot, mcData)
       bot.pathfinder.setMovements(moves)
 
+      // AUTH
       auth(bot)
 
       setTimeout(() => {
         bot.chat(`/login ${config.bot.password}`)
-      }, 5000)
+      }, 3000)
 
+      // MODULES
       movement(bot)
       combat(bot)
       antiAfk(bot)
 
-      leaveRejoin(bot, startBot)
     })
 
     bot.on('end', () => {
-      if (restarting) return
-      restarting = true
+      console.log("🔄 reconnecting...")
+      setTimeout(startBot, 5000)
+    })
 
-      setTimeout(() => {
-        restarting = false
-        startBot()
-      }, 20000)
+    bot.on('error', err => {
+      console.log("❌ ERROR:", err.message)
     })
 
     bot.on('error', err => {
       console.log("Error:", err.message)
     })
 
-  } catch (e) {
-    console.log("Crash:", e.message)
-    setTimeout(startBot, 20000)
+ } catch (err) {
+    console.log("❌ CRASH:", err.message)
   }
 }
+
 
 startBot()
